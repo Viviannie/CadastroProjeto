3fc1d7bb0ea0bec9de4548605ceaac750347fcd2
Viviannie <viviannie@gmail.com>
2024-06-19 16:42:57 -0300

    Primeiro commit

---
+++ cadastro/src/main/java/model/entidades/Vendedor.java
@@ -0,0 +1,13 @@
+package model.entidades;
+
+public class Vendedor {
+
+	private Integer id;
+    private String name;
+    private String email;
+    private Date birthdate;
+    private Double baseSalary;
+
+    private Department department;
+	
+}
